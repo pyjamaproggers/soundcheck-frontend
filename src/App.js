56ab925e3684be82a6f0d1125096745807014f4d
iamaryanyadavv <aryanyadav1601@gmail.com>
2023-06-29 23:22:27 +0530

    installed nextui and material ui

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/HomePage/homePage'
 import Login from './pages/LoginPage/loginPage';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react';
+
 
 function App() {
     const randomInt = Math.floor((Math.random() * 100) + 1);
     return ( 
         <>
-        <Routes>
-                <Route path='/' component={Home} />
-                <Route path='/login' component={Login} />
-        </Routes>
+        <NextUIProvider>
+            <Routes>
+                    <Route path='/' component={Home} />
+                    <Route path='/login' component={Login} />
+            </Routes>
+        </NextUIProvider>
         </>
     );
 }
