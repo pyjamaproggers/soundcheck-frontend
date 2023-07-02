e9ea7d5cf2a3235718654fdd7bf2491f98a1cf9c
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-07-02 15:08:49 +0530

    Designed Homescreen Grid

--- src/pages/HomePage/homePage.js
+++ src/pages/HomePage/homePage.js
@@ -1,10 +1,108 @@
-import { Grid, Text } from "@nextui-org/react";
+import { Grid, Image } from "@nextui-org/react";
 import React from "react";
+import TempLogo from "../HomePage/TempLogo.jpeg";
 
-export default function Home() {
-    return(
-        <Grid.Container>
-            <Text css={{color: 'white', height: '200vh'}}></Text>
+export default function HomeScreen() {
+  return (
+    <Grid.Container
+      direction="column"
+      css={{
+        height: "200vh",
+        width: "100vw",
+      }}
+    >
+      <Grid.Container
+        css={{ height: "50vh", width: "100vw", position: "relative" }}
+        direction="row"
+      >
+        <Grid.Container
+          direction="column"
+          css={{
+            height: "50vh",
+            width: "25vw",
+          }}
+        >
+          <Image
+            css={{ height: "25vh", width: "25vw", position: "relative" }}
+            src={TempLogo}
+            objectFit="fill"
+          />
+          <Image
+            css={{ height: "25vh", width: "25vw", position: "relative" }}
+            src={TempLogo}
+            objectFit="fill"
+          />
         </Grid.Container>
-    )
+
+        <Image
+          css={{ height: "50vh", width: "50vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Grid.Container
+          direction="column"
+          css={{
+            height: "50vh",
+            width: "25vw",
+          }}
+        >
+          <Image
+            css={{ height: "25vh", width: "25vw", position: "relative" }}
+            src={TempLogo}
+            objectFit="fill"
+          />
+          <Image
+            css={{ height: "25vh", width: "25vw", position: "relative" }}
+            src={TempLogo}
+            objectFit="fill"
+          />
+        </Grid.Container>
+      </Grid.Container>
+      <Grid.Container
+        css={{ height: "50vh", width: "100vw", position: "relative" }}
+        direction="row"
+      >
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+        <Image
+          css={{ height: "25vh", width: "25vw", position: "relative" }}
+          src={TempLogo}
+          objectFit="fill"
+        />
+      </Grid.Container>
+    </Grid.Container>
+  );
 }
