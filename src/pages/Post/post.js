7460bff16e23041f69bb62773e021c225529ddf2
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-07-13 02:02:30 +0530

    First version of website ready for deploy

--- src/pages/Post/post.js
+++ src/pages/Post/post.js
@@ -52,79 +52,121 @@ export default function Post() {
     const editorState = EditorState.createWithContent(contentState);
     const htmlContent = stateToHTML(contentState);
 
-    // Function to replace links with embeds
-    const replaceLinks = (content) => {
-      // Function to extract the tweet link from the content
-      const extractTweetLink = (text) => {
-        const regex = /(https:\/\/twitter\.com\/\w+\/status\/\d+)/;
-        const match = text.match(regex);
-        return match ? match[1] : null;
-      };
-
-      // Function to extract the YouTube video link from the content
-      const extractYouTubeLink = (text) => {
-        const regex = /https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&?\n\s<]+)/;
-        const match = text.match(regex);
-        return match ? match[1] : null;
-      };
-
-      // Function to extract the Instagram post link from the content
-      const extractInstagramLink = (text) => {
-        const regex = /(https:\/\/www\.instagram\.com\/p\/\w+)/;
-        const match = text.match(regex);
-        return match ? match[1] : null;
-      };
-
-      // Function to extract the Spotify link from the content
-      const extractSpotifyLink = (text) => {
-        const regex = /(https:\/\/open\.spotify\.com\/\w+\/\w+\S+)/;
-        const match = text.match(regex);
-        return match ? match[1] : null;
-      };
-
-      // Function to extract the Apple Music link from the content
-      const extractAppleMusicLink = (text) => {
-        const regex = /\/(in\/album\/.*)/;
-        const match = text.match(regex);
-        return match ? match[1] : null;
-      };
-
-      const tweetLink = extractTweetLink(content);
-      const youTubeLink = extractYouTubeLink(content);
-      const instagramLink = extractInstagramLink(content);
-      const spotifyLink = extractSpotifyLink(content);
-      const appleMusicLink = extractAppleMusicLink(content);
-
-      // Replace tweet link with Twitter embed
-      if (tweetLink) {
-        content = content.replace(tweetLink, `<div><TwitterTweetEmbed tweetId="${tweetLink}" /></div>`);
-      }
-
-      // Replace YouTube link with YouTube video embed
-      if (youTubeLink) {
-        content = content.replace(youTubeLink, `<div><iframe width="560" height="315" src="https://www.youtube.com/embed/${youTubeLink}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>`);
-      }
-
-      // Replace Instagram link with Instagram post embed
-      if (instagramLink) {
-        content = content.replace(instagramLink, `<div><blockquote className="instagram-media" data-instgrm-permalink="${instagramLink}" data-instgrm-version="13" style={{ background: "#FFF", border: "0", borderRadius: "3px", boxShadow: "0 0 1px 0 rgba(0,0,0,0.5)", display: "block", margin: "1px", maxWidth: "540px", minWidth: "326px", padding: "0", width: "calc(100% - 2px)" }}></blockquote></div>`);
-      }
-
-      // Replace Spotify link with Spotify embed
-      if (spotifyLink) {
-        content = content.replace(spotifyLink, `<div><iframe style={{ borderRadius: '12px' }} src="${spotifyLink}" width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></div>`);
-      }
-
-      // Replace Apple Music link with Apple Music embed
-      if (appleMusicLink) {
-        content = content.replace(appleMusicLink, `<div><iframe style={{ width: "70%" }} allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0" height="175" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/${appleMusicLink}"></iframe></div>`);
-      }
-
-      return content;
+    // Function to extract the tweet link from the post description
+    const extractTweetLinks = (content) => {
+      const regex = /(https:\/\/twitter\.com\/\w+\/status\/\d+)/g;
+      const matches = content.match(regex);
+      return matches ? matches : [];
     };
 
+    // Function to extract the YouTube video link from the post description
+    const extractYouTubeLinks = (content) => {
+      const regex = /https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&?\n\s<]+)/g;
+      const matches = content.match(regex);
+      return matches ? matches : [];
+    };
+
+    // Function to extract the Instagram post link from the post description
+    const extractInstagramLinks = (content) => {
+      const regex = /(https:\/\/www\.instagram\.com\/p\/\w+)/g;
+      const matches = content.match(regex);
+      return matches ? matches : [];
+    };
+
+    // Function to extract the Spotify link from the post description
+    const extractSpotifyLinks = (content) => {
+      const regex = /(https:\/\/open\.spotify\.com\/\w+\/\w+\S+)/g;
+      const matches = content.match(regex);
+      return matches ? matches : [];
+    };
+
+    // Function to extract the Apple Music link from the post description
+    const extractAppleMusicLinks = (content) => {
+      const regex = /\/(in\/album\/.*)/g;
+      const matches = content.match(regex);
+      return matches ? matches : [];
+    };
+
+    // Function to replace all occurrences of the tweet link with the TwitterTweetEmbed component
+    const replaceTweetLinks = (content) => {
+      const regex = /(https:\/\/twitter\.com\/\w+\/status\/\d+)/g;
+      return content.replace(regex, (match) => {
+        const tweetId = extractTweetId(match);
+        return `<TwitterTweetEmbed tweetId="${tweetId}" />`;
+      });
+    };
+
+    // Function to replace all occurrences of the YouTube link with the YouTube video embed
+    const replaceYouTubeLinks = (content) => {
+      const regex = /https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([^&?\n\s<]+)/g;
+      return content.replace(regex, (match, videoId) => {
+        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+      });
+    };
+
+    // Function to replace all occurrences of the Instagram link with the Instagram post embed
+    const replaceInstagramLinks = (content) => {
+      const regex = /(https:\/\/www\.instagram\.com\/p\/\w+)/g;
+      return content.replace(regex, (match, postUrl) => {
+        return `<blockquote className="instagram-media" data-instgrm-permalink="${postUrl}" data-instgrm-version="13" style={{ background: "#FFF", border: "0", borderRadius: "3px", boxShadow: "0 0 1px 0 rgba(0,0,0,0.5)", display: "block", margin: "1px", maxWidth: "540px", minWidth: "326px", padding: "0", width: "calc(100% - 2px)" }}></blockquote>`;
+      });
+    };
+
+    // Function to replace all occurrences of the Spotify link with the Spotify embed
+    const replaceSpotifyLinks = (content) => {
+      const regex = /(https:\/\/open\.spotify\.com\/\w+\/\w+\S+)/g;
+      return content.replace(regex, (match, spotifyUrl) => {
+        return `<iframe style={{ borderRadius: '12px' }} src="${spotifyUrl}" width="100%" height="352" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`;
+      });
+    };
+
+    // Function to replace all occurrences of the Apple Music link with the Apple Music embed
+    const replaceAppleMusicLinks = (content) => {
+      const regex = /\/(in\/album\/.*)/g;
+      return content.replace(regex, (match, albumUrl) => {
+        return `<iframe style={{ width: "70%" }} allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameBorder="0" height="175" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/${albumUrl}"></iframe>`;
+      });
+    };
+
+    // Function to extract the tweet ID from the tweet link
+    const extractTweetId = (link) => {
+      const regex = /\/(\d+)$/;
+      const match = link.match(regex);
+      return match ? match[1] : null;
+    };
+
+    // Get the tweet links from the post description
+    const tweetLinks = extractTweetLinks(htmlContent);
+    // Get the YouTube video links from the post description
+    const youTubeLinks = extractYouTubeLinks(htmlContent);
+    // Get the Instagram post links from the post description
+    const instagramLinks = extractInstagramLinks(htmlContent);
+    // Get the Spotify links from the post description
+    const spotifyLinks = extractSpotifyLinks(htmlContent);
+    // Get the Apple Music links from the post description
+    const appleMusicLinks = extractAppleMusicLinks(htmlContent);
+
+    let replacedContent = htmlContent;
+    tweetLinks.forEach((link) => {
+      replacedContent = replacedContent.replace(link, replaceTweetLinks(link));
+    });
+    youTubeLinks.forEach((link) => {
+      replacedContent = replacedContent.replace(link, replaceYouTubeLinks(link));
+    });
+    instagramLinks.forEach((link) => {
+      replacedContent = replacedContent.replace(link, replaceInstagramLinks(link));
+    });
+    spotifyLinks.forEach((link) => {
+      replacedContent = replacedContent.replace(link, replaceSpotifyLinks(link));
+    });
+    appleMusicLinks.forEach((link) => {
+      replacedContent = replacedContent.replace(link, replaceAppleMusicLinks(link));
+    });
+
+    replacedContent = replacedContent.replace(/https:\/\/music\.apple\.com/g, '');
+
     const renderedContent = (
-      <div dangerouslySetInnerHTML={{ __html: replaceLinks(htmlContent) }} />
+      <div dangerouslySetInnerHTML={{ __html: replacedContent }} />
     );
 
     return (
@@ -284,6 +326,19 @@ export default function Post() {
           <Text css={{ fontWeight: '$semibold', fontSize: '$lg', margin: '8px 0px' }}>
             MORE BY SoundCheck™
           </Text>
+
+          <Row css={{
+            padding: '0px 4px 0px 0px',
+            borderStyle: 'solid',
+            borderWidth: '2px',
+            borderColor: 'rgb(20,20,20)',
+            margin: '8px',
+          }}>
+            <Image src={Divine} width={400} css={{ height: '91px', objectFit: 'cover' }} />
+            <Text className="multiline-ellipsis" css={{ fontWeight: '$semibold', fontSize: '$md', height: '100%', paddingLeft: '4px' }}>
+              DUMMY ARTICLE TITLE DUMMY ARTICLE TITLE DUMMY ARTICLE TITLE DUMMY ARTICLE TITLE
+            </Text>
+          </Row>
 
           <Row css={{
             padding: '0px 4px 0px 0px',
