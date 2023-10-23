62112f7f8ca45109f381d8af95daa32f5898e831
Zahaan Shapoorjee <zahaan@shapoorjee.com>
2023-10-23 17:31:04 +0530

    LOL

--- src/pages/Post/post.js
+++ src/pages/Post/post.js
@@ -376,7 +376,7 @@ export default function Post() {
                             {publishedPosts.map((post, index) => {
                                 if (index <= 6) {
                                     return (
-                                        <Grid css={{ width: '100%', cursor: 'pointer', marginRight:"4px", backgroundColor: 'rgb(20,20,20)',}} onClick={() => {
+                                        <Grid css={{ width: '100%', cursor: 'pointer', marginRight:"4px",}} onClick={() => {
                                             navigate(`/posts/${post._id}`);
                                         }}>
                                             
